ab5aa28a807bd5927fb7c2477527d629ebebb380
mango dyl <dylanbonds@hotmail.com>
2018-12-17 21:21:28 -0600

    js setup, button added

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+$(function() {
+
+    var correctAnswers = 0
+    var q1 = false;
+    var q2 = false;
+    var q3 = false;
+
+
+    $(".btn").on("click", function() {
+
+        
+
+    });
+
+});
