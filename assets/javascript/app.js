f13206f01cae5b337c74a4da0ca54b2f0beac717
mango dyl <dylanbonds@hotmail.com>
2018-12-18 16:27:38 -0600

    js started. q1 almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,45 @@ $(function() {
     var q2 = false;
     var q3 = false;
 
+    var number = 20;
+    var intervalID;
+
+    function timer() {
+
+    };
+
+
+    // --- Quiz start 
 
     $(".btn").on("click", function() {
 
-        
+        $("#start-button").addClass("hidden");
+
+        $("#question1").removeClass("hidden");
+
+    });
+
+    // --- Question 1
+
+    $(".option").on("click", function() {
+
+        if ($(this).hasClass("correct")) {
+
+            $("#answer-title").text("Correct!");
+
+            console.log(this, "ok");
+        }
+        else {
+
+            $("#answer-title").text("Wrong!");
+
+            $("#answer-text").text("Correct!");
+
+        };
+
+        $("#question1").addClass("hidden");
+
+        $("#answer-div").removeClass("hidden");
 
     });
 
