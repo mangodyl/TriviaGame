502043556b716a9815720031fa2f04c29f0035c4
mango dyl <dylanbonds@hotmail.com>
2018-12-18 16:58:55 -0600

    timer function and answer q1 done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,10 +6,18 @@ $(function() {
     var q2 = false;
     var q3 = false;
 
-    var number = 20;
-    var intervalID;
+    var number = 21;
+    var intervalId;
+
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(timer, 1000);
+      }
 
     function timer() {
+        number--;
+
+        $("#timer-div").html("<h2>" + number + "</h2");
 
     };
 
@@ -22,23 +30,31 @@ $(function() {
 
         $("#question1").removeClass("hidden");
 
+        run();
+
     });
 
     // --- Question 1
 
-    $(".option").on("click", function() {
+    $(".option").on("click", function timeOut() {
 
         if ($(this).hasClass("correct")) {
 
             $("#answer-title").text("Correct!");
 
-            console.log(this, "ok");
+            $("#gif").attr("src", 'assets/images/dean-right.gif');
+
+            $("#answer-text").text("Oh man, well played! Even if it was a complete guess, here you are with a right answer so good job. Starting off strong.");
+
+            correctAnswers++;
         }
         else {
 
             $("#answer-title").text("Wrong!");
 
-            $("#answer-text").text("Correct!");
+            $("#gif").attr("src", 'assets/images/snl-wrong.gif')
+
+            $("#answer-text").text("Hah, just as I predicted. I did mention that this was gonna be pretty unfair...");
 
         };
 
